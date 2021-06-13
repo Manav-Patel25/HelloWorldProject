bf1a0753d3e1aba79c6d9f9566ee0d73d37b2d1e
Manav-Patel25 <85610984+Manav-Patel25@users.noreply.github.com>
2021-06-13 13:56:01 +0530

    Add files via upload

---
+++ positiveinteger.py
@@ -0,0 +1,9 @@
+Python 3.8.8 (default, Apr 13 2021, 15:08:03) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:/Users/snugs/Desktop/positiveinteger.py ==============
+Print the positive number from the given list
+LIST1=12,-7,5,64,-14
+12 5 64 LIST2=12,14,-95,3
+12 14 3 
+>>> 
